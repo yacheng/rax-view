29da9a19da2e8555f0d6f179328a21b0e4c1b220
recover758126 <761647716@qq.com>
2019-08-24 00:09:42 +0800

    fix: remove _nativeNode and reduce code

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,25 @@
-import {
-  createElement,
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-} from 'rax';
+import { createElement, forwardRef, CSSProperties, ForwardRefExoticComponent, RefAttributes, HTMLAttributes } from 'rax';
 import { isWeex } from 'universal-env';
-import { ViewProps } from './types';
 
-const styles = {
-  initial: {
-    border: '0 solid black',
-    boxSizing: 'border-box',
-    display: 'flex',
-    flexDirection: 'column',
-    alignContent: 'flex-start',
-    flexShrink: 0
-  }
+const defaultStyle: CSSProperties = {
+  border: '0 solid black',
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'column',
+  alignContent: 'flex-start',
+  flexShrink: 0
 };
 
-const View: Rax.RefForwardingComponent<{}, ViewProps> = forwardRef(
-  (props, ref) => {
-    const viewRef = useRef(null);
-    useImperativeHandle(ref, () => ({
-      _nativeNode: viewRef.current,
-    }));
+export type ViewProps = RefAttributes<HTMLDivElement> & HTMLAttributes<HTMLDivElement>;
 
+const View: ForwardRefExoticComponent<ViewProps> = forwardRef(
+  (props, ref) => {
     if (isWeex) {
-      return <div ref={viewRef} {...props} />;
+      return <div ref={ref} {...props} />;
     } else {
       const { style = {}, ...rest } = props;
-      let styleProps = {
-        ...styles.initial,
-        ...style
-      };
       return (
-        <div ref={viewRef} style={styleProps as Rax.CSSProperties} {...rest} />
+        <div ref={ref} style={{...defaultStyle, ...style}} {...rest} />
       );
     }
   }
