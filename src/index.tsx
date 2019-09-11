3bbb920f75f4e8eb9bca4c677c71c8263eb5c2b8
recover758126 <761647716@qq.com>
2019-09-11 12:22:15 +0800

    refactor: component
    - fix miniapp style
    - use stylesheet file

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,14 @@
-import { createElement, forwardRef, CSSProperties, ForwardRefExoticComponent, RefAttributes, HTMLAttributes } from 'rax';
+import { createElement, forwardRef, ForwardRefExoticComponent, RefAttributes, HTMLAttributes } from 'rax';
+import cx from 'classnames/dedupe';
 import { isWeex } from 'universal-env';
-
-const defaultStyle: CSSProperties = {
-  border: '0 solid black',
-  boxSizing: 'border-box',
-  display: 'flex',
-  flexDirection: 'column',
-  alignContent: 'flex-start',
-  flexShrink: 0
-};
+import './index.css';
 
 export type ViewProps = RefAttributes<HTMLDivElement> & HTMLAttributes<HTMLDivElement>;
 
 const View: ForwardRefExoticComponent<ViewProps> = forwardRef(
   (props, ref) => {
-    if (isWeex) {
-      return <div ref={ref} {...props} />;
-    } else {
-      const { style = {}, ...rest } = props;
-      return (
-        <div ref={ref} style={{...defaultStyle, ...style}} {...rest} />
-      );
-    }
+    const { className, style, ...rest } = props;
+    return <div {...rest} ref={ref} className={cx( isWeex ? '' : 'rax-view', className)} style={style} />;
   }
 );
 export default View;
