9b58c9f86efd2178a01bf5185a2c3aa18b1591bc
stephenlee.lk <stephenlee.lk@alibaba-inc.com>
2019-09-25 16:41:18 +0800

    fix: dist file to source file

---
+++ src/miniapp-wechat/index.ts
@@ -0,0 +1,55 @@
+import fmtEvent from './fmtEvent';
+
+Component({
+  data: {},
+  props: {
+    className: '',
+    style: '',
+    defaultStyle:
+      'border:0 solid black;display:flex;flex-direction:column;align-content:flex-start;flex-shrink:0;box-sizing:border-box;',
+    onClick: e => {},
+    onLongpress: e => {},
+    onAppear: e => {},
+    onDisAppear: e => {},
+    onTouchStart: e => {},
+    onTouchMove: e => {},
+    onTouchEnd: e => {},
+    onTouchCancel: e => {},
+    animation: null
+  },
+  didMount() {},
+  methods: {
+    onClick(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onClick(event);
+    },
+    onLongpress(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onLongpress(event);
+    },
+    onAppear(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onAppear(event);
+    },
+    onDisAppear(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onDisAppear(event);
+    },
+    onTouchStart(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onTouchStart(event);
+    },
+    onTouchMove(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onTouchMove(event);
+    },
+    onTouchEnd(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onTouchEnd(event);
+    },
+    onTouchCancel(e) {
+      const event = fmtEvent(this.props, e);
+      this.props.onTouchCancel(event);
+    }
+  }
+});
